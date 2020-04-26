f901c6d22d43dd080b9c84b5f31eb7138c69010f
Todd Birchard <toddbirchard@gmail.com>
2020-04-25 21:49:30 -0400

    Prevent assets from building in development mode.

--- application/assets.py
+++ application/assets.py
@@ -3,13 +3,13 @@ from flask_assets import Environment, Bundle
 
 def compile_assets(app):
     """Compile stylesheets if `app` is running in development mode."""
-    assets = Environment(app)
-    Environment.auto_build = True
-    Environment.debug = False
-    less_bundle = Bundle('less/*.less',
-                         filters='less,cssmin',
-                         output='dist/css/styles.css',
-                         extra={'rel': 'stylesheet/less'})
-    assets.register('less_all', less_bundle)
     if app.config['FLASK_ENV'] == 'development':
+        assets = Environment(app)
+        Environment.auto_build = True
+        Environment.debug = False
+        less_bundle = Bundle('less/*.less',
+                             filters='less,cssmin',
+                             output='dist/css/styles.css',
+                             extra={'rel': 'stylesheet/less'})
+        assets.register('less_all', less_bundle)
         less_bundle.build(force=True)
